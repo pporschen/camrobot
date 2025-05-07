e1e2e511033a05b284150b3dc190d414180f622b
Patrick <accounts@pp83.de>
2025-05-07 16:28:40 +0200

    update dependencies and add WebcamCapture component for webcam functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import "./App.css";
+import WebcamCapture from "./components/WebcamCapture";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<WebcamCapture />
+		</div>
+	);
 }
 
 export default App;
