2e5eae8b87c37f52a8432ae6f3b826e6fa297166
Patrick <accounts@pp83.de>
2025-05-08 20:17:22 +0200

    add moveAxis function and integrate AXIS enum into moveArm utility

--- src/utils/moveArm.ts
+++ src/utils/moveArm.ts
@@ -1,4 +1,9 @@
 const ARM_SPEED = 0.2; // Speed of the arm in m/s
+enum AXIS {
+	X = 1,
+	Y = 2,
+	Z = 3,
+}
 
 const moveArmToCoords = ({ x, y, z }: { x: number; y: number; z: number }) => {
 	const command = { T: 104, x, y, z, t: 3.14, spd: ARM_SPEED };
@@ -13,4 +18,18 @@ const moveArmToCoords = ({ x, y, z }: { x: number; y: number; z: number }) => {
 	});
 };
 
-export default moveArmToCoords;
+const moveAxis = (axis: AXIS) => {
+	const command = { T: 123, m: 1, axis, cmd: 0, spd: ARM_SPEED };
+	const queryParams = encodeURIComponent(JSON.stringify(command));
+	const url = `/js?json=${queryParams}`;
+
+	fetch(url, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+};
+
+const moveArmUtils = { moveArmToCoords, moveAxis, AXIS };
+export default moveArmUtils;
