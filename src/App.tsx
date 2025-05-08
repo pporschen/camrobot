ba40e1c9b0db029878ceba56210d497e746c87c1
Patrick <accounts@pp83.de>
2025-05-08 09:24:04 +0200

    refactor App component to improve styling structure and include ShowIpAddress

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import WebcamCapture from "./components/WebcamCapture";
 import ButtonPanelRight from "./components/Buttons/ButtonPanelRight";
 import ButtonPanelLeft from "./components/Buttons/ButtonPanelLeft";
+import ShowIpAddress from "./components/ShowIPAddress";
+
+const appStyles = {
+	width: "100vw",
+	height: "100vh",
+	overflow: "hidden", // Prevents scrollbars
+};
 
 function App() {
 	return (
-		<div
-			className="App"
-			style={{
-				width: "100vw",
-				height: "100vh",
-				overflow: "hidden", // Prevents scrollbars
-			}}
-		>
+		<div className="App" style={appStyles}>
 			<ButtonPanelLeft />
 			<WebcamCapture />
 			<ButtonPanelRight />
+			<ShowIpAddress />
 		</div>
 	);
 }
